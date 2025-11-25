78d5d00aac8bcea46a4bd78c50a9d2c7807e8a06
Kiran <padolekiran2001@address.com>
2025-11-25 11:54:40 +0530

    task compleated

---
+++ src/components/PageLayout.js
@@ -0,0 +1,23 @@
+// export default function PageLayout({header, footer, children}) {
+//     return (
+//         <div>
+//             <header>{header}</header>
+//             <main>{children}</main>
+//             <footer>{footer}</footer>
+//         </div>
+//     )
+// }
+
+
+import React from "react";
+
+export default function PageLayout({ header, footer, children }) {
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  );
+}
+
